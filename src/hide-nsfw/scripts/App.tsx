77c77f35eeb7122e796c890ad71ca7c0e0cd2318
vrcalpha <190100403+vrcalphabet@users.noreply.github.com>
2026-03-04 01:04:05 +0900

    a

--- src/hide-nsfw/scripts/App.tsx
+++ src/hide-nsfw/scripts/App.tsx
@@ -1,9 +1,11 @@
 import { Toggle } from './components/Toggle'
+import style from './styles/style.css?inline'
 
 export function App() {
   return (
     <>
-      <span>Hello, PReact!</span>
+      <style>{style}</style>
+      <span class="text-red-500 font-bold animate-ping">Hello, PReact!</span>
       <Toggle />
     </>
   )
