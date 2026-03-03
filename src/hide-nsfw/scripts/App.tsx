55d0baf56dcdeeedf89ced8c5bfb1373f759893f
vrcalpha <190100403+vrcalphabet@users.noreply.github.com>
2026-03-03 17:07:21 +0900

    a

--- src/hide-nsfw/scripts/App.tsx
+++ src/hide-nsfw/scripts/App.tsx
@@ -1,10 +1,11 @@
 import { Toggle } from './components/Toggle'
+import { Tailwind } from 'react-shadow-scope'
 
 export function App() {
   return (
-    <div>
+    <Tailwind href="./style/style.css">
       <span>Hello, PReact!</span>
       <Toggle />
-    </div>
+    </Tailwind>
   )
 }
